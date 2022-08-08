fc7a9bada08f63835759208f03698cbf4176425e
ragnarzone <ragnarzone@gmail.com>
2022-08-08 12:24:42 +0300

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/com/ragnarzone/chernyshovpetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package com.ragnarzone.chernyshovpetclinic.services;
+
+import com.ragnarzone.chernyshovpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}
