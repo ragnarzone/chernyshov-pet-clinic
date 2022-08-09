d78a1265ee68ab496fa4e0230a56f11c2bd1f36d
ragnarzone <ragnarzone@gmail.com>
2022-08-09 14:59:35 +0300

    added vet list and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/ragnarzone/chernyshovpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/ragnarzone/chernyshovpetclinic/controllers/VetController.java
@@ -1,13 +1,25 @@
 package com.ragnarzone.chernyshovpetclinic.controllers;
 
+import com.ragnarzone.chernyshovpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/vets")
 @Controller
 public class VetController {
 
-    @RequestMapping({"/vets","/vets/index", "/vets/index.html"})
-    public String listVets() {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+    @RequestMapping({"","/","/index", "/index.html"})
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
+
         return "vets/index";
     }
 }
