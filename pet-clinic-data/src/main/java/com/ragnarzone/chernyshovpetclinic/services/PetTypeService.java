6a28be278650c8f282383c3427f076fa59e8e70e
ragnarzone <ragnarzone@gmail.com>
2022-08-11 13:47:01 +0300

    added PetTypeService. Closes #29

---
+++ pet-clinic-data/src/main/java/com/ragnarzone/chernyshovpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.ragnarzone.chernyshovpetclinic.services;
+
+import com.ragnarzone.chernyshovpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
