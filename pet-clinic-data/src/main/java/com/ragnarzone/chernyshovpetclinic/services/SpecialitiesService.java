b19d3c33ecc8f252ab9d1323774e0347d7c61237
ragnarzone <ragnarzone@gmail.com>
2022-08-12 08:44:34 +0300

    added Specialities Map Impl. Closes #31

---
+++ pet-clinic-data/src/main/java/com/ragnarzone/chernyshovpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.ragnarzone.chernyshovpetclinic.services;
+
+import com.ragnarzone.chernyshovpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
