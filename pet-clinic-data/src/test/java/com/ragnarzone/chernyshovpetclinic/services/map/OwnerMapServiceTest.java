77dd6e634545b4e37caaa87375b89ae0ab328366
ragnarzone <ragnarzone@gmail.com>
2022-08-26 21:58:11 +0300

    added test to find all in OwnerMapServiceTest

---
+++ pet-clinic-data/src/test/java/com/ragnarzone/chernyshovpetclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,46 @@
+package com.ragnarzone.chernyshovpetclinic.services.map;
+
+import com.ragnarzone.chernyshovpetclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OwnerMapServiceTest {
+
+    OwnerMapService ownerMapService;
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(1L).build());
+    }
+
+    @Test
+    void findAll() {
+        Set<Owner> ownerSet = ownerMapService.findAll();
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
